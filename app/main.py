b969b006ddb76174590c7a32f61c33d0f2705863
Katherine <katherinegm9@gmail.com>
2024-07-28 16:27:50 -0400

    Implement HTTP response with status line

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 import socket
 
 def main():
@@ -7,7 +6,12 @@ def main():
 
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept()  # wait for client
+
+    # Send HTTP response
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    connection.sendall(response)
+    connection.close()
 
 if __name__ == "__main__":
     main()
